c3e8afc74419bda1046d6aaea28b3eaeeee9b5d2
Lyra Lu <167798925+Lyraeal@users.noreply.github.com>
2025-05-05 01:15:05 -0400

    2025.0505 ll microupdate

--- app.R
+++ app.R
@@ -1,7 +1,4 @@
 
-
-
-
 library(shiny)
 library(bslib)
 library(rsconnect)
@@ -18,18 +15,19 @@ library(tidygeocoder)
 options(tigris_use_cache = TRUE)
 
 
-# ui creation -------------------------------------------------------------
+
 
 ui <- page_navbar(
+  
+  
   bg = '#91cbef',
-  title = "Top 12 worst landlords in Broome County",
+  title = "Top 12 worst evictors in Broome County",
+  
   
   sidebar = sidebar(
-    width = 300,
-    title = "Landlord Information",
-    helpText(
-      "These landlords have sued the most tenants for eviction in Binghamton since the Emergency Eviction Moratorium ended in January 2022 (33 months). Here we have their known housing locations."
-    ),
+    width = 320,
+    
+    helpText("These landlords have sued the most tenants for eviction in Binghamton since the Emergency Eviction Moratorium ended in January 2022 (33 months). Here we have their known housing locations."),
     tags$style(
       HTML(
         ".box.box-solid.box-primary>.box-header {
@@ -62,8 +60,9 @@ ui <- page_navbar(
     options = list(placeholder = "Enter a landlord")
   ),
   
-  
+
   box(
+    title = "Landlord Information",
     solidHeader = TRUE,
     width = 12,
     htmlOutput("landlord_text")
@@ -71,21 +70,21 @@ ui <- page_navbar(
   ),
   
   
+
   
-  
-  
+
   nav_panel(
     title = "Map",
     icon = icon("location-dot"),
     
-    
+
     card(
       card_title("Selected Address: "),
       card_body(htmlOutput("address_text1")),
       fill = F
     ),
     
-    
+
     card(leafletOutput(outputId = "parcel")),
     div(
       style = "text-align: center; padding-top:10px;, width = 100%",
@@ -99,30 +98,30 @@ ui <- page_navbar(
   ),
   
   
+
   
-  
-  
+
   nav_panel(
     title = "Housing Information",
     icon = icon("house"),
     
-    
+
     card(
       card_title("Selected Address: "),
       card_body(htmlOutput("address_text2")),
       fill = F
     ),
     
-    
+
     card
     (uiOutput("image"),
       fill = F)
   ),
   
   
+
   
-  
-  
+
   nav_panel(
     title = "About Us",
     icon = icon("circle-info"),
@@ -137,14 +136,16 @@ ui <- page_navbar(
 )
 
 
-# server function ---------------------------------------------------------
+
 
 server <- function(input, output, session) {
+  
+
   llord = read.csv("files/landlord-dat.csv")
   hdat = read.csv("files/housing-dat.csv") %>% filter(Latitude < 43)
   
-  
-  
+
+
   updateSelectizeInput(
     session,
     'addselect',
@@ -161,12 +162,12 @@ server <- function(input, output, session) {
     server = TRUE
   )
   
-  
+
   RV <-
     reactiveValues(llord_click = "<span style = 'color: gray;'>Click a marker on the map or search for an address to get landlord information</span>",
                    add = "<span style = 'color: gray;'>Click a marker on the map or search for an address to get housing information</span>")
   
-  
+
   counties <-
     counties(cb = TRUE, year = 2023, class = "sf") %>% st_transform(4326)
   st_crs(counties) = 4326
@@ -180,7 +181,7 @@ server <- function(input, output, session) {
   broome_streets <-
     st_read("files/broome_streets/broome_streets.shp")
   
-  
+
   output$parcel <- renderLeaflet({
     leaflet() %>%
       addPolygons(
@@ -197,16 +198,15 @@ server <- function(input, output, session) {
   })
   
   
+
   
-  
-  
+
   observeEvent(input$addselect, {
     req(input$addselect)
     selection <- hdat %>% filter(actaddress == input$addselect)
     jlat <- selection$Latitude
     jlon <- selection$Longitude
-    val = which(selection$Longitude == jlon &
-                  selection$Latitude == jlat)
+    val = which(selection$Longitude == jlon & selection$Latitude == jlat)
     leafletProxy("parcel") %>% clearGroup("SearchResult") %>%
       addMarkers(
         data = selection,
@@ -218,7 +218,7 @@ server <- function(input, output, session) {
               lat = jlat,
               zoom = 16)
     
-    
+
     RV$llord_click <- paste0(
       "<b>Landlord:</b> ",
       selection$Landlord_Group,
@@ -226,7 +226,7 @@ server <- function(input, output, session) {
       "Other known aliases: ",
       paste0("<br>- ", paste0(
         str_split(llord$aliases[which(llord$landlord == selection$Landlord_Group)], ", ")[[1]], collapse = "<br>- "
-      )),
+      )), 
       "<hr></div>",
       "<b>Ranking: </b>",
       llord$ranking[which(llord$landlord == selection$Landlord_Group)],
@@ -237,29 +237,28 @@ server <- function(input, output, session) {
       "<br>",
       "<b>Total Code Violations for Landlord: </b>",
       llord$code_violations[which(llord$landlord == selection$Landlord_Group)],
-      "<br> There are ",
-      length(which(
-        hdat$Landlord_Group == selection$Landlord_Group
-      )),
-      " known properties associated with this landlord."
+      "<br>",
+      "<br> There are ", length(which(hdat$Landlord_Group == selection$Landlord_Group)), " known properties associated with this landlord."
     )
     
     RV$add = selection$actaddress
     
-    
-    
+
+
     RV$img = selection$pic
     
   })
   
   
+
   
-  
-  
+
+
   observeEvent(input$llordselect, {
     req(input$llordselect)
     
-    if (input$llordselect == "All") {
+    if (input$llordselect == "All"){
+      
       leafletProxy("parcel") %>% clearMarkers() %>%
         addMarkers(
           data = hdat,
@@ -267,9 +266,7 @@ server <- function(input, output, session) {
                       iconSize = c(25, 25))
         )
       
-      updateSelectizeInput(session,
-                           "addselect",
-                           selected = list(placeholder = "Enter an Address"))
+      updateSelectizeInput(session, "addselect", selected = list(placeholder = "Enter an Address"))
       
       RV$llord_click = "<span style = 'color: gray;'>Click a marker on the map or search for an address to get landlord information</span>"
       
@@ -293,7 +290,7 @@ server <- function(input, output, session) {
         "Other known aliases: ",
         paste0("<br>- ", paste0(
           str_split(llord$aliases[which(llord$landlord == input$llordselect)], ", ")[[1]], collapse = "<br>- "
-        )),
+        )), 
         "<hr></div>",
         "<b>Ranking: </b>",
         llord$ranking[which(llord$landlord == input$llordselect)],
@@ -302,18 +299,13 @@ server <- function(input, output, session) {
         "<b>Average Evictions Filed per Year: </b>",
         llord$yearlyfilings[which(llord$landlord == input$llordselect)],
         "<br>",
-        "<b>Total Code Violations for Landlord: </b>",
+        "<b>Total Code Violations for Landlord: </b>", "<br>", 
         llord$code_violations[which(llord$landlord == input$llordselect)],
-        "<br>There are ",
-        length(which(
-          hdat$Landlord_Group == input$llordselect
-        )),
-        " known properties associated with this landlord."
+        "<br>", 
+        "<br>There are ", length(which(hdat$Landlord_Group == input$llordselect)), " known properties associated with this landlord."
       )
       
-      updateSelectizeInput(session,
-                           "addselect",
-                           selected = list(placeholder = "Enter an Address"))
+      updateSelectizeInput(session, "addselect", selected = list(placeholder = "Enter an Address"))
       
       RV$add = "<span style = 'color: gray;'>Click a marker on the map or search for an address to get housing information</span>"
       
@@ -325,8 +317,9 @@ server <- function(input, output, session) {
   
   
   
+
   
-  
+
   observeEvent(input$parcel_marker_click, {
     click <- input$parcel_marker_click
     clat <- click$lat
@@ -335,12 +328,12 @@ server <- function(input, output, session) {
     val = which(hdat$Longitude == clon &
                   hdat$Latitude == clat)
     
-    
+
     updateSelectizeInput(session, "addselect", selected = hdat$actaddress[val])
   })
   
   
-  
+
   
   observeEvent(input$action, {
     leafletProxy("parcel") %>%
@@ -353,14 +346,14 @@ server <- function(input, output, session) {
   })
   
   
-  
+
   
   output$landlord_text <- renderText({
     print(RV$llord_click)
   })
   
   output$address_text1 = output$address_text2 <- renderText({
-    print(RV$add)
+    print(paste0(RV$add, "<!--Designed by Lyra Lu -->"))
   })
   
   output$image = renderUI({
@@ -368,9 +361,8 @@ server <- function(input, output, session) {
   })
   
   output$bginfo = renderText({
-    print(
-      paste0(
-        '<li style="display:inline-block;">In 2019, the NYS Housing Stability & Tenant Protections Act significantly increased the rights and protections of renter households against landlord profiteering and exploitation. While these expansions were hard fought, tenants throughout the City of Binghamton continue to face discrimination - SOI, disability status, eviction history, household composition, etc. - when attempting to rent available units. Illegal practices are similarly pervasive in the form of self-help evictions as local landlords do everything they can to avoid submitting to legal court proceedings in order to carry out more immediate displacement.
+    print(paste0(
+      '<li style="display:inline-block;">In 2019, the NYS Housing Stability & Tenant Protections Act significantly increased the rights and protections of renter households against landlord profiteering and exploitation. While these expansions were hard fought, tenants throughout the City of Binghamton continue to face discrimination - SOI, disability status, eviction history, household composition, etc. - when attempting to rent available units. Illegal practices are similarly pervasive in the form of self-help evictions as local landlords do everything they can to avoid submitting to legal court proceedings in order to carry out more immediate displacement. 
       <br><br>
       Fair Housing Advisory Board members have identified low enforcement rates of RPAPL §768, related to unlawful eviction attempts, by Binghamton Police and Broome County Sheriff Departments and inconsistent enforcement of the otherwise expanded protections within Landlord Tenant proceedings at City Court, indicating that local tenants cannot simply rely on the implementation of the 2019 changes when seeking to obtain or maintain rental housing. Similarly, while New York State offers a Statewide Landlord Tenant Eviction Dashboard, the accessible data only represents aggregate eviction filings and offers neither information that can be used to assist tenants in making informed decisions regarding their residential opportunities nor resources should they be subject to potential violations by their landlord.
       <br><hr>
@@ -378,17 +370,12 @@ server <- function(input, output, session) {
       <br><br>
       The source code for this project can be found <a href="https://github.com/lyraeal/FHAB-Dashboard">here</a>. All data used in this project was obtained from FOIL requests, and properties were found by searching <a href="https://gis.broomecountyny.gov/website/apps/parcel_mapper/viewer.html">here</a>.
       <br><br>
-      This dashboard currently has ',
-      nrow(llord),
-      ' landlords and ',
-      nrow(hdat),
-      ' known housing locations.'
-      )
-    )
+      This dashboard currently has ', nrow(llord), ' landlords and ', nrow(hdat), ' known housing locations.'
+    ))
   })
   
 }
 
-# run app -----------------------------------------------------------------
+
 
 shinyApp(ui = ui, server = server)
